ba026f0b429ac8c991584bda2549e65dee4a74dc
Théo <118971209+AsTheLow@users.noreply.github.com>
2022-12-11 19:46:20 +0100

    update

--- projet_GO1.go
+++ projet_GO1.go
@@ -3,12 +3,21 @@ package main
 import (
     "fmt"
     "log"
+    "os"
+    "strings"
 
     "github.com/OJ/gobuster"
+    "github.com/Ullaakut/nmap"
 )
 
 func main() {
-    gb := gobuster.New("https://www.example.com")
+    if len(os.Args) < 2 {
+        log.Fatal("Usage: go run main.go <TARGET_URL>")
+    }
+
+    target := os.Args[1]
+
+    gb := gobuster.New(target)
     gb.Wordlist = "/usr/share/wordlists/dirbuster/common.txt"
     gb.Threads = 20
 
@@ -16,5 +25,35 @@ func main() {
         log.Fatal(err)
     }
 
-    fmt.Printf("Open ports: %v\n", gb.OpenPorts())
+    fmt.Printf("Open ports (gobuster): %v\n", gb.OpenPorts())
+
+    scanner, err := nmap.NewScanner(
+        nmap.WithTargets(strings.Split(target, ",")...),
+        nmap.WithPorts("1-65535"),
+        nmap.WithServiceInfo(),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    result, err := scanner.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    openPorts := []string{}
+    for _, host := range result.Hosts {
+        if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+            continue
+        }
+
+        address := host.Addresses[0]
+        for _, port := range host.Ports {
+            if port.State.State == "open" {
+                openPorts = append(openPorts, fmt.Sprintf("%s:%d/%s", address, port.ID, port.Service.Name))
+            }
+        }
+    }
+
+    fmt.Printf("Open ports (nmap): %v\n", openPorts)
 }

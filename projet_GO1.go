c20b4cdb49d6b0c9933440302d633e9773a30ddb
Théo <118971209+AsTheLow@users.noreply.github.com>
2022-12-11 19:50:05 +0100

    spf13/cobra

--- projet_GO1.go
+++ projet_GO1.go
@@ -3,57 +3,67 @@ package main
 import (
     "fmt"
     "log"
-    "os"
     "strings"
 
     "github.com/OJ/gobuster"
     "github.com/Ullaakut/nmap"
+    "github.com/spf13/cobra"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        log.Fatal("Usage: go run main.go <TARGET_URL>")
-    }
+    var target string
+    var wordlist string
+    var threads int
 
-    target := os.Args[1]
+    rootCmd := &cobra.Command{
+        Use:   "port-scanner",
+        Short: "Scan a target for open ports",
+        Run: func(cmd *cobra.Command, args []string) {
+            gb := gobuster.New(target)
+            gb.Wordlist = wordlist
+            gb.Threads = threads
 
-    gb := gobuster.New(target)
-    gb.Wordlist = "/usr/share/wordlists/dirbuster/common.txt"
-    gb.Threads = 20
-
-    if err := gb.Start(); err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Printf("Open ports (gobuster): %v\n", gb.OpenPorts())
-
-    scanner, err := nmap.NewScanner(
-        nmap.WithTargets(strings.Split(target, ",")...),
-        nmap.WithPorts("1-65535"),
-        nmap.WithServiceInfo(),
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    result, err := scanner.Run()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    openPorts := []string{}
-    for _, host := range result.Hosts {
-        if len(host.Ports) == 0 || len(host.Addresses) == 0 {
-            continue
-        }
-
-        address := host.Addresses[0]
-        for _, port := range host.Ports {
-            if port.State.State == "open" {
-                openPorts = append(openPorts, fmt.Sprintf("%s:%d/%s", address, port.ID, port.Service.Name))
+            if err := gb.Start(); err != nil {
+                log.Fatal(err)
             }
-        }
+
+            fmt.Printf("Open ports (gobuster): %v\n", gb.OpenPorts())
+
+            scanner, err := nmap.NewScanner(
+                nmap.WithTargets(strings.Split(target, ",")...),
+                nmap.WithPorts("1-65535"),
+                nmap.WithServiceInfo(),
+            )
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            result, err := scanner.Run()
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            openPorts := []string{}
+            for _, host := range result.Hosts {
+                if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+                    continue
+                }
+
+                address := host.Addresses[0]
+                for _, port := range host.Ports {
+                    if port.State.State == "open" {
+                        openPorts = append(openPorts, fmt.Sprintf("%s:%d/%s", address, port.ID, port.Service.Name))
+                    }
+                }
+            }
+
+            fmt.Printf("Open ports (nmap): %v\n", openPorts)
+        },
     }
 
-    fmt.Printf("Open ports (nmap): %v\n", openPorts)
-}
+    rootCmd.Flags().StringVarP(&target, "target", "t", "", "The target URL to scan")
+    rootCmd.Flags().StringVarP(&wordlist, "wordlist", "w", "/usr/share/wordlists/dirbuster/common.txt", "The path to the wordlist to use")
+    rootCmd.Flags().IntVarP(&threads, "threads", "T", 20, "The number of worker threads to use")
+
+    if err := rootCmd.Execute(); err != nil {
+        log.Fatal(err

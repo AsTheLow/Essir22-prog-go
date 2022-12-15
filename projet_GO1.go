b3d5edf865cca50ede36f5600cb4d561f19a0229
Théo <118971209+AsTheLow@users.noreply.github.com>
2022-12-15 13:30:57 +0100

    Update projet_GO1.go

--- projet_GO1.go
+++ projet_GO1.go
@@ -3,66 +3,70 @@ package main
 import (
     "fmt"
     "log"
-    "os/exec"
-    "strings"
+    "os"
+
+    "github.com/spf13/cobra"
 
     "github.com/OJ/gobuster"
-    "github.com/Ullaakut/nmap"
-    "github.com/spf13/cobra"
+    "github.com/OJ/gobuster/v3/libgobuster"
+)
+
+var (
+    rootCmd = &cobra.Command{
+        Use:   "my-cli",
+        Short: "My command-line interface",
+        Long:  "A sample CLI application using gobuster and spf13/cobra",
+        Run: func(cmd *cobra.Command, args []string) {
+            // Print usage information if no arguments are provided
+            if len(args) == 0 {
+                cmd.Usage()
+                return
+            }
+
+            // Get the target URL and wordlist from the arguments
+            url := args[0]
+            wordlist := args[1]
+
+            // Enumerate directories and files on the web server using gobuster
+            enumerateWebServer(url, wordlist)
+        },
+    }
 )
 
 func main() {
-    var target string
-    var threads int
+    // Define flags for the root command
+    rootCmd.Flags().StringP("target", "t", "", "target host or IP address")
+    rootCmd.Flags().StringSliceP("ports", "p", []string{}, "target port(s)")
+    rootCmd.Flags().IntP("workers", "w", 10, "number of concurrent workers")
+    rootCmd.Flags().BoolP("quiet", "q", false, "don't show banner and logo")
 
-    rootCmd := &cobra.Command{
-        Use:   "port-scanner",
-        Short: "Scan a target for open ports",
-        Run: func(cmd *cobra.Command, args []string) {
-            gb := gobuster.New(target)
-            gb.Wordlist = "/usr/share/wordlists/dirbuster/common.txt"
-            gb.Threads = threads
+    // Parse command-line arguments
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
 
-            if err := gb.Start(); err != nil {
-                log.Fatal(err)
-            }
+func enumerateWebServer(url string, wordlist string) {
+    // Create a new gobuster instance
+    gb := gobuster.New(url, wordlist, &libgobuster.Options{
+        Threads:  10,
+        Quiet:    true,
+        Verbose:  false,
+        NoColor:  false,
+        ShowIPs:  false,
+        Recursive: false,
+        WildcardForced: false,
+    })
 
-            fmt.Printf("Open ports (gobuster): %v\n", gb.OpenPorts())
+    // Enumerate directories and files on the web server
+    err := gb.Start()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-            scanner, err := nmap.NewScanner(
-                nmap.WithTargets(strings.Split(target, ",")...),
-                nmap.WithPorts("1-65535"),
-                nmap.WithServiceInfo(),
-            )
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            result, err := scanner.Run()
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            openPorts := []string{}
-            for _, host := range result.Hosts {
-                if len(host.Ports) == 0 || len(host.Addresses) == 0 {
-                    continue
-                }
-
-                address := host.Addresses[0]
-                for _, port := range host.Ports {
-                    if port.State.State == "open" {
-                        openPorts = append(openPorts, fmt.Sprintf("%s:%d/%s", address, port.ID, port.Service.Name))
-                    }
-                }
-            }
-
-            fmt.Printf("Open ports (nmap): %v\n", openPorts)
-
-            if out, err := exec.Command("zap-cli", "--zap-url", "http://localhost:8080", "status").Output(); err != nil {
-                log.Fatal(err)
-            } else {
-                fmt.Printf("OWASP ZAP status: %s\n", out)
-            }
-
-            if out, err := exec.Command("openvas-check-setup").Output();
+    // Print the results
+    for _, result := range gb.Results() {
+        fmt.Println(result.String())
+    }
+}
